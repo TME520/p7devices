54b4c9049e1ee732208ea3fc2d3cb80501b17371
Five Twenty <tme520@gmail.com>
2020-08-25 11:32:47 +1000

    Python code + startup shell script

---
+++ bstick_nano/bstick.py
@@ -0,0 +1,79 @@
+import os
+import time
+import re
+import urllib.request
+import urllib.parse
+import json
+import traceback
+from argparse import ArgumentParser
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+publicname='BlinkStick'
+version='0.1'
+
+# Path to data folder (contains ML stuff)
+cb1DataFolder = os.environ.get('CB1DATAFOLDER')
+if not os.path.exists(cb1DataFolder):
+    os.makedirs(cb1DataFolder)
+
+def callURL(url2call, creds):
+    try:
+        print(Fore.LIGHTGREEN_EX + 'callURL')
+        url = url2call
+        req = urllib.request.Request(url, headers=creds)
+        response = urllib.request.urlopen(req)
+        payload = response.read()
+        return payload
+    except urllib.error.HTTPError:
+        print(Fore.RED + '[HTTPError] Failed to call ' + str(url) + '\nProvider might be down or credentials might have expired.')
+        return 'HTTPERROR'
+        pass
+    except urllib.error.URLError:
+        print(Fore.RED + '[URLError] Failed to call ' + str(url) + '\nNetwork connection issue (check Internet access).')
+        return "URLERROR"
+        pass
+
+def dynamodbTableCheck(databaseURL, tableName):
+    try:
+        print(Fore.LIGHTGREEN_EX + 'dynamodbTableCheck')
+        dynamodb = boto3.client('dynamodb', endpoint_url=databaseURL)
+        response = dynamodb.describe_table(TableName=tableName)
+    # except dynamodb.exceptions.ResourceNotFoundException:
+    except Exception as e:
+        print('[DEBUG] DynamoDB table ' + tableName + ' not found')
+        response = 'Table not found'
+        # traceback.print_exc()
+        pass
+    return str(response)
+
+def dynamodbListTableItems(databaseURL, tableName):
+    try:
+        print(Fore.LIGHTGREEN_EX + 'dynamodbListTableItems')
+        dynamodb = boto3.resource('dynamodb', endpoint_url=databaseURL)
+        tableToList = dynamodb.Table(tableName)
+        tableToList.scan()
+        response = tableToList.scan()
+    except Exception as e:
+        print('[ERROR] Failed to list content of database table ' + tableName + '.\n', e)
+        response = 'Table listing failed'
+        traceback.print_exc()
+        pass
+    return response
+
+def writeDataToFile(targetFile, dataToWrite, successMsg, failureMsg, mode):
+    print(Fore.LIGHTGREEN_EX + 'writeDataToFile')
+    try:
+        if mode == 'overwrite':
+            newCB1File = open(targetFile,'w+')
+        elif mode == 'append':
+            newCB1File = open(targetFile,'a')
+        newCB1File.write(dataToWrite)
+        newCB1File.close()
+        print(successMsg)
+    except Exception as e:
+        print(failureMsg, e)
+        traceback.print_exc()
+        pass
+
---
+++ bstick_nano/startBStick.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo 'Setting environment variables:'
+echo -e '\t- BlinkStick instance identification'
+export NAME=''
+export NICKNAME=''
+
+echo -e '\t- Credentials'
+
+echo -e '\t- Parameters...'
+export DYNAMODBURL='http://localhost:8001'
+export CB1DATAFOLDER='./bstick_data/'
+export LOGSFOLDER='./log/'
+export LOGFILENAME='bstick_default.log'
+export CONFIGFILE='sample_config'
+
+echo -e '\t- Preferences...'
+# 0 = OFF
+# 1 = ON
+export ENABLEBSTICK=1
+
+echo 'Starting BlinkStick instance '$NAME' ($NICKNAME)'
+python3 ./bstick.py
+
+exit 0
